8158a0ef7a03f9dd4e0ddf1a52953ee480ed48cb
USER <USER@DESKTOP-28934GB>
2019-06-04 18:00:52 +0900

    20190604

--- CrawlNews/src/main/java/com/crawlnews/study/controller/NewsWebController.java
+++ CrawlNews/src/main/java/com/crawlnews/study/controller/NewsWebController.java
@@ -2,17 +2,23 @@ package com.crawlnews.study.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.crawlnews.study.repository.MongoRepository;
+
 @Controller
 public class NewsWebController {
 
+	@Autowired
+	MongoRepository mongoRepository;
+	
 	@RequestMapping("/news")
 	public String newsPage() {
 		
-		return "test";
+		return "newsKeywordSearch";
 	}
 	
 	@RequestMapping("/news/search")
@@ -23,6 +29,8 @@ public class NewsWebController {
 		String searchDate = tempString.replace("-", ".");
 		System.out.println(searchDate);
 		System.out.println(searchCategory);
+		
+		
 		
 		return "test";
 	}
--- CrawlNews/src/main/java/com/crawlnews/study/repository/MongoRepository.java
+++ CrawlNews/src/main/java/com/crawlnews/study/repository/MongoRepository.java
@@ -1,6 +1,7 @@
 package com.crawlnews.study.repository;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,5 @@ import com.crawlnews.study.domain.MongoDomain;
 @Repository
 public interface MongoRepository extends org.springframework.data.mongodb.repository.MongoRepository<MongoDomain, String>{
 	List<MongoDomain> findAll();
+	//Map<String, Integer> findByDateAndCategory(String date, String category);
 }
